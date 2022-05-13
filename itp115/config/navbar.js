76a2fb0815fe3f73ed3a14e25e94254f1e506a12
Jenny Kim <yjkim@usc.edu>
2022-05-13 15:31:35 -0700

    fixed link

--- itp115/config/navbar.js
+++ itp115/config/navbar.js
@@ -1,3 +1,5 @@
 import {piazza_link} from './config.js';
 
+let piazza = document.querySelector("#piazza");
 document.querySelector("#piazza").setAttribute("href", piazza_link);
+// console.log(document.querySelector("#piazza"));
